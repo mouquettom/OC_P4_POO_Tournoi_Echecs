efcdf9daa8bc41478879bb85b6ae31e59d9cf8b6
Tom Mouquet <mouquettom@gmail.com>
2024-12-13 10:21:43 +0100

    Amélioration du parcours utilisateur dans le fichier : OC_P4_POO_Tournoi_Echecs_V2.py

--- model/model.py
+++ model/model.py
@@ -1,14 +1,13 @@
-from random import choice, shuffle
 from datetime import datetime, time
+from random import choice, shuffle
 
 
 class Joueur:
 
-    def __init__(self, prenom, nom, genre, date_naissance):
+    def __init__(self, prenom, nom, genre):
         self.prenom = prenom
         self.nom = nom
         self.genre = genre
-        self.date_naissance = date_naissance
         self.points = 0.0
 
     def vainqueur_match(self):
@@ -43,11 +42,9 @@ class Match:
 
 class Tournoi:
 
-    def __init__(self, nom, lieu, date_debut, date_fin):
+    def __init__(self, nom, lieu):
         self.nom = nom
         self.lieu = lieu
-        self.date_debut = date_debut
-        self.date_fin = date_fin
         self.joueurs = []
         self.matchs = []
         self.joueurs_restants = []
@@ -60,12 +57,16 @@ class Tournoi:
 
     def match_making(self):
         if len(self.joueurs_restants) < 2:
-            raise ValueError("Il n'y a pas assez de joueurs pour organiser un match.")
+            raise ValueError("Pas assez de joueurs pour oraniser un match.")
 
-        self.joueurs_restants.sort(key=lambda joueur: joueur.points, reverse=True)
+        def tri_points_decroissant(joueurs):
+            return -joueurs.points
+        #self.joueurs_restants.sort(lambda joueur: -joueur.points)
+
+        self.joueurs_restants.sort(key=tri_points_decroissant)
 
         j1, j2 = self.joueurs_restants.pop(0), self.joueurs_restants.pop(0)
-        return j1, j2
+        return j1,j2
 
     def jouer_tour(self):
         self.joueurs_restants = self.joueurs.copy()
@@ -82,58 +83,37 @@ class Tournoi:
 
             T1.ajouter_match(M)
 
-    def afficher_classement(self):
-        self.joueurs.sort(key=lambda joueur: joueur.points, reverse=True)
-        print(f"\nClassement final :")
-        print()
-        for index, joueur in enumerate(self.joueurs, start=1):
-            print(f"{index}. {joueur}")
 
 print()
-print("*** TOURNOI D'ECHECS ***")
+print("*** LE TOURNOI D'ECHEC ***")
 print()
 
 nom_tournoi = input("Le nom du Tournoi : ")
 lieu_tournoi = input("Le lieu du Tournoi : ")
-date_debut_tournoi = input("La date de début (JJ/MM/AAAA) : ")
-date_debut_obj = datetime.strptime(date_debut_tournoi,"%d/%m/%Y")
-date_fin_tournoi = input("La date de fin (JJ/MM/AAAA) : ")
-date_fin_obj = datetime.strptime(date_fin_tournoi, "%d/%m/%Y")
-T1 = Tournoi(nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi)
+T1 = Tournoi(nom_tournoi, lieu_tournoi)
 
-print()
 while True:
     user_choice = ""
 
     while user_choice not in ["Y", "N"]:
-        user_choice = input("Souhaitez-vous ajouter un joueur (Y/N) ? ")
+        user_choice = input("Voulez-vous ajouter un joueur ? ")
 
     if user_choice == "Y":
-        prenom_joueur = input("Quel est le prénom du joueur : ")
-        nom_joueur = input("Quel est le nom de famille du joueur : ")
-        genre_joueur = input("Quel est le genre du joueur (H/F) : ")
-        date_naissance = input("Quelle est la date de naissance du joueur (JJ/MM/AAAA) : ")
-        date_naissance_obj = datetime.strptime(date_naissance, "%d/%m/%Y")
-        joueur = Joueur(prenom_joueur, nom_joueur, genre_joueur, date_naissance_obj)
+        prenom = input("Le prénom du joueur ? ")
+        nom = input("Le nom du joueur ? ")
+        genre = input("Le genre du joueur (H/F) ? ")
+        joueur = Joueur(prenom, nom, genre)
         T1.ajouter_joueur(joueur)
-        print()
-    elif user_choice == "N":
+    else:
         break
 
 shuffle(T1.joueurs)
 
-print()
-tour = int(input("Veuillez indiquer le nombre de tours : "))
-
-print("\nLe Tournoi commence :")
-print()
+tour = int(input("Veuillez indiquer le nombre de tours ? "))
 for i in range(tour):
     T1.jouer_tour()
     print()
 
-print("\nScores finaux :")
-print()
+print(f"\nScores finaux :")
 for joueur in T1.joueurs:
     print(joueur)
-
-T1.afficher_classement()
